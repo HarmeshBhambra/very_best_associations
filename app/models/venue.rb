54f5a26df23d5b9d84a672b81b02bae96d49f262
HarmeshBhambra <harmeshbhambra@wireless-s0-so-150-33-3.uchicago.edu>
2015-11-18 12:51:06 -0600

    changes

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,9 @@
 class Venue < ActiveRecord::Base
+  validates :name, :presence => true, :uniqueness => { :scope => :address }
+
+  validates :neighborhood_id, :presence => true
+
+  has_many :favorites
+
+  belongs_to :neighborhood
 end
